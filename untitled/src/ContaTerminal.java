e4e88bf0dc3d7c6ea1fb7313892d1739f323d83a
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-06 17:00:43 -0300

    ContaTerminal.java

---
+++ untitled/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int digito;
+        int conta = 1021;
+        String nomeCliente = "MARIO ANDRADE";
+        String agencia;
+        Double saldo = 237.48;
+
+        System.out.println("Por favor, digite o número da Agência e depois o digito: ");
+        agencia = sc.next();
+
+        System.out.println("Digito");
+        digito = sc.nextInt();
+
+
+        System.out.println("Olá, " + nomeCliente +", obrigado por criar uma conta em nosso banco, " +
+        "sua agência é: " +  agencia + "-" + digito  + ", conta " + conta + " e seu saldo " + saldo);
+
+        sc.close();
+    }
+}
